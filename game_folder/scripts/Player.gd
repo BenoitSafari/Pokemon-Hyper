d56af99fe256bce184de3ba9e8aa81b997f2aeec
BenoitSafari <gregory.michalak.192@gmail.com>
2022-09-09 00:33:18 +0200

    Created Player scene
    Walk animation and Script created for Player scene

--- game_folder/scripts/Player.gd
+++ game_folder/scripts/Player.gd
@@ -1,4 +1,33 @@
 extends KinematicBody2D
 
+onready var anim_tree = $PlayerAnimTree
+onready var anim_state = $PlayerAnimTree.get("parameters/playback")
+
+export var speed = 100.0
+
+var input_vector = Vector2(0,0)
+
+
 func _ready():
-	pass
+	anim_tree.active = true
+	animate_player()
+
+func _physics_process(_delta: float) -> void:
+	input_vector = Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	)
+	move_player()
+	animate_player()
+
+
+func move_player() -> void:
+	var _player_motion = move_and_slide(input_vector.normalized() * speed)
+
+func animate_player() -> void:
+	if input_vector == Vector2(0,0):
+		anim_state.travel("Idle")
+	else:
+		anim_tree.set("parameters/Walk/blend_position", input_vector)
+		anim_tree.set("parameters/Idle/blend_position", input_vector)
+		anim_state.travel("Walk")
